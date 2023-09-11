c77a64ba54feb05afb99ddadfe524a71d7160a3b
Akdeniz Akdogan <Akdeniz Akdogan@AKDENIZ>
2023-09-11 23:27:26 +0300

    Initial Commit

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,49 @@
 package com.trendyol.shipment;
 
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+
+        if (products.size() < 3) {
+
+            ShipmentSize biggestSize = ShipmentSize.SMALL;
+
+            for (Product product : products) {
+                if (product.getSize().ordinal() > biggestSize.ordinal()) {
+                    biggestSize = product.getSize();
+                }
+            }
+
+            return biggestSize;
+        }
+
+        if (products.stream().allMatch(x -> x.getSize() == ShipmentSize.X_LARGE)) {
+            return ShipmentSize.X_LARGE;
+        }
+
+        Map<ShipmentSize, Long> groupedProducts = products.stream().collect(groupingBy(Product::getSize, counting()));
+        Stream<Map.Entry<ShipmentSize, Long>> sizesBiggerThenThreshold = groupedProducts.entrySet().stream().filter(x -> x.getValue() >= 3);
+        Map.Entry<ShipmentSize, Long> sizeCountEntry = sizesBiggerThenThreshold.findFirst().orElse(null);
+        ShipmentSize biggestSize;
+
+        if (sizeCountEntry != null) {
+            biggestSize = sizeCountEntry.getKey().next();
+        } else {
+            OptionalInt maxEnumIndex = groupedProducts.keySet().stream().mapToInt(Enum::ordinal).max();
+            biggestSize = ShipmentSize.values()[maxEnumIndex.orElse(0)];
+        }
+
+        return biggestSize;
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,15 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    private static final ShipmentSize[] shipmentSizes = values();
+
+    public ShipmentSize next() {
+        int nextIndex = this.ordinal() + 1 == shipmentSizes.length ? this.ordinal() : this.ordinal() + 1;
+
+        return shipmentSizes[nextIndex];
+    }
 }
+
+
