9ebeaf69b385b83ed238ee392dc55f39a582d1c5
Akdeniz Akdogan <Akdeniz Akdogan@AKDENIZ>
2023-09-11 23:53:53 +0300

    Refactor

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,50 +1,12 @@
 package com.trendyol.shipment;
 
-import java.util.List;
-import java.util.Map;
-import java.util.OptionalInt;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
-
-    public ShipmentSize getShipmentSize() {
-
-        if (products.size() < 3) {
-
-            ShipmentSize biggestSize = ShipmentSize.SMALL;
-
-            for (Product product : products) {
-                if (product.getSize().ordinal() > biggestSize.ordinal()) {
-                    biggestSize = product.getSize();
-                }
-            }
-
-            return biggestSize;
-        }
-
-        if (products.stream().allMatch(x -> x.getSize() == ShipmentSize.X_LARGE)) {
-            return ShipmentSize.X_LARGE;
-        }
-
-        Map<ShipmentSize, Long> groupedProducts = products.stream().collect(groupingBy(Product::getSize, counting()));
-        Stream<Map.Entry<ShipmentSize, Long>> sizesBiggerThenThreshold = groupedProducts.entrySet().stream().filter(x -> x.getValue() >= 3);
-        Map.Entry<ShipmentSize, Long> sizeCountEntry = sizesBiggerThenThreshold.findFirst().orElse(null);
-        ShipmentSize biggestSize;
-
-        if (sizeCountEntry != null) {
-            biggestSize = sizeCountEntry.getKey().next();
-        } else {
-            OptionalInt maxEnumIndex = groupedProducts.keySet().stream().mapToInt(Enum::ordinal).max();
-            biggestSize = ShipmentSize.values()[maxEnumIndex.orElse(0)];
-        }
-
-        return biggestSize;
-    }
+    private static final int SHIPMENT_SIZE_THRESHOLD = 3;
 
     public List<Product> getProducts() {
         return products;
@@ -52,5 +14,37 @@ public class Basket {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    public ShipmentSize getShipmentSize() {
+        if (products.size() < SHIPMENT_SIZE_THRESHOLD) {
+            return findBiggestSize();
+        }
+
+        var shipmentSizeCountMap = products.stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+        var sizeCountEntry = shipmentSizeCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() >= SHIPMENT_SIZE_THRESHOLD)
+                .findFirst();
+
+        var biggestSize = sizeCountEntry.map(entry -> entry.getKey().next())
+                .orElse(getMaxSizeFromShipmentSizeCountMap(shipmentSizeCountMap));
+
+        return biggestSize;
+    }
+
+    private ShipmentSize findBiggestSize() {
+        return products.stream()
+                .map(Product::getSize)
+                .max(Comparator.comparing(Enum::ordinal))
+                .orElse(ShipmentSize.SMALL);
+    }
+
+    private ShipmentSize getMaxSizeFromShipmentSizeCountMap(Map<ShipmentSize, Long> shipmentSizeCountMap) {
+        OptionalInt maxEnumIndex = shipmentSizeCountMap.keySet().stream()
+                .mapToInt(Enum::ordinal)
+                .max();
+        return ShipmentSize.values()[maxEnumIndex.orElse(0)];
     }
 }
